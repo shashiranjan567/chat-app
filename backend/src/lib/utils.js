84843fc7549592d25574d167d62976b1798dba0d
shashiranjan567 <shashiranjan94314@gmail.com>
2025-09-07 14:36:02 +0530

    Updated the url

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,10 +8,10 @@ export const generateToken = (userId, res) => {
     })
 
     res.cookie("jwt", token, {
-        maxage: 30 * 24 * 60 * 60 * 1000, // ms
+        maxAge: 30 * 24 * 60 * 60 * 1000, // ms
         httpOnly: true,  // prevent cookie from being accessed by client side js
-        sameSite: "strict",// cookie will only be sent in a first-party context
-        secure: process.env.NODE_ENV !== "development"
+        sameSite: "none", // allow cross-site cookies
+        secure: true // always secure in production
     })
     return token;
 }
