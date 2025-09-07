8b370c37cdc113d0e871cb6a091bd5926a4e39ae
shashiranjan567 <shashiranjan94314@gmail.com>
2025-09-07 14:03:11 +0530

    Updated chat app with new features/fixes

--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -57,7 +57,7 @@ const SettingsPage = () => {
                       J
                     </div>
                     <div>
-                      <h3 className="font-medium text-sm">Rajiv Kumar</h3>
+                      <h3 className="font-medium text-sm">Shashi Ranjan</h3>
                       <p className="text-xs text-base-content/70">Online</p>
                     </div>
                   </div>
